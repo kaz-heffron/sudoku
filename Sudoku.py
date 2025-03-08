5d11dd6fc8ff281d54bad35e492aa7ea08e4f701
unknown <lheffron@alt.app>
2025-03-08 14:18:02 -0600

    revised number input

--- Sudoku.py
+++ Sudoku.py
@@ -14,16 +14,21 @@ grid_width = 550
 grid_height = 550
 sudokuBlockSize = 66
 rect_list= []
-nums = [0,0,0,0,0,0,0,0,0, #indexes 0-8
-            0,0,0,0,0,0,0,0,0, #indexes 9-17
-            0,0,0,0,0,0,0,0,0, #indexes 18-26
-            0,0,0,0,0,0,0,0,0, #indexes 27-35
-            0,0,0,0,0,0,0,0,0, #indexes 36-44
-            0,0,0,0,0,0,0,0,0, #indexes 45-53
-            0,0,0,0,0,0,0,0,0, #indexes 54-62
-            0,0,0,0,0,0,0,0,0, #indexes 63-71
-            0,0,0,0,0,0,0,0,0,] #indexes 72-80
+key_var = ''
+nums = ['','','','','','','','','', #indexes 0-8
+            '','','','','','','','','', #indexes 9-17
+            '','','','','','','','','',#indexes 18-26
+            '','','','','','','','','', #indexes 27-35
+            '','','','','','','','','', #indexes 36-44
+            '','','','','','','','','', #indexes 45-53
+            '','','','','','','','','', #indexes 54-62
+            '','','','','','','','','', #indexes 63-71
+            '','','','','','','','',''] #indexes 72-80
 color = pygame.Color(120,95,30)
+
+
+
+
 
 def drawSudokuGrid():
     for x in range(10, grid_width + 10, sudokuBlockSize):
@@ -39,16 +44,17 @@ def drawSudokuGrid():
         cor += 197
 
 def print_numbers():
-    #one row represents on COLUMN
     
     font = pygame.font.SysFont(None, 24)
     i = 0
 
+    #nums
     for x in range(10, grid_width + 10, sudokuBlockSize):
         for y in range(10, grid_height + 10, sudokuBlockSize):
             img = font.render(str(nums[i]), True, pygame.Color(120,95,30))
             screen.blit(img, (x+27, y+25))
             i+=1
+            
             
 running = True
 while running:
@@ -58,15 +64,37 @@ while running:
             
             if event.type == pygame.QUIT: 
                 running = False
-    
-            elif event.type == pygame.MOUSEBUTTONUP:
+
+            if event.type == pygame.KEYDOWN:  
+                if event.key == pygame.K_0:
+                    key_var = ''               
+                if event.key == pygame.K_1:
+                    key_var = '1'               
+                if event.key == pygame.K_2:
+                    key_var = '2'      
+                if event.key == pygame.K_3:
+                    key_var = '3'
+                if event.key == pygame.K_4:
+                    key_var = '4'
+                if event.key == pygame.K_5:
+                    key_var = '5'
+                if event.key == pygame.K_6:
+                    key_var = '6'
+                if event.key == pygame.K_7:
+                    key_var = '7'
+                if event.key == pygame.K_8:
+                    key_var = '8'
+                if event.key == pygame.K_9:
+                    key_var = '9'
+                
+
+            if event.type == pygame.MOUSEBUTTONUP:
                     mouse_pos = pygame.mouse.get_pos()
                     for item in rect_list:
                         if item.collidepoint(mouse_pos):
-                            chosen_num = input("input #")
                             for i in range(len(nums)):
                                 if i == rect_list.index(item):
-                                    nums[i]=chosen_num
+                                    nums[i]=str(key_var)
                                 
                             print(str(item))
                             print(str(nums))
@@ -76,6 +104,20 @@ while running:
             screen.fill(pygame.Color(232, 196, 144))
             drawSudokuGrid()
             print_numbers()
+            
+            font = pygame.font.SysFont(None, 17)
+            secondary_font = pygame.font.SysFont(None, 22)
+            tertiary_font = pygame.font.SysFont(None, 14)
+
+            selected_text = font.render('selected number: ', True, color)
+            screen.blit(selected_text, (620,20))
+
+            text_surface = secondary_font.render(key_var, True, color)
+            screen.blit(text_surface, (720,18))
+
+            selected_text = tertiary_font.render('(click 0 to clear cell)', True, color)
+            screen.blit(selected_text, (620,35))
+
             pygame.display.flip()
 
 pygame.quit()
