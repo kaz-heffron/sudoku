c8c2440d05180491ad533e5552e84f8d045db3a0
unknown <lheffron@alt.app>
2025-05-05 11:16:52 -0500

    marking completely finished, new game button created

--- Sudoku.py
+++ Sudoku.py
@@ -29,11 +29,47 @@ medium = pygame.Rect(400,300,50,50)
 hard = pygame.Rect(500,300,50,50)
 marking = False
 mark_button = pygame.Rect(620,130,40,30)
+new_g_button = pygame.Rect(43,623,80,23)
 p_num = 0
 marked_list = [[0,0,0,0,0,0,0,0,0,] for _ in range(81)]
 
 
 
+rows = [
+    [0,  9, 18, 27, 36, 45, 54, 63, 72],
+    [1, 10, 19, 28, 37, 46, 55, 64, 73],
+    [2, 11, 20, 29, 38, 47, 56, 65, 74],
+    [3, 12, 21, 30, 39, 48, 57, 66, 75],
+    [4, 13, 22, 31, 40, 49, 58, 67, 76],
+    [5, 14, 23, 32, 41, 50, 59, 68, 77],
+    [6, 15, 24, 33, 42, 51, 60, 69, 78],
+    [7, 16, 25, 34, 43, 52, 61, 70, 79],
+    [8, 17, 26, 35, 44, 53, 62, 71, 80]
+]
+ 
+columns = [
+    [ 0,  1,  2,  3,  4,  5,  6,  7,  8],
+    [ 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    [18, 19, 20, 21, 22, 23, 24, 25, 26],
+    [27, 28, 29, 30, 31, 32, 33, 34, 35],
+    [36, 37, 38, 39, 40, 41, 42, 43, 44],
+    [45, 46, 47, 48, 49, 50, 51, 52, 53],
+    [54, 55, 56, 57, 58, 59, 60, 61, 62],
+    [63, 64, 65, 66, 67, 68, 69, 70, 71],
+    [72, 73, 74, 75, 76, 77, 78, 79, 80]
+]
+ 
+quadrants = [
+    [ 0,  1,  2,  9, 10, 11, 18, 19, 20],
+    [ 3,  4,  5, 12, 13, 14, 21, 22, 23],
+    [ 6,  7,  8, 15, 16, 17, 24, 25, 26],
+    [27, 28, 29, 36, 37, 38, 45, 46, 47],
+    [30, 31, 32, 39, 40, 41, 48, 49, 50],
+    [33, 34, 35, 42, 43, 44, 51, 52, 53],
+    [54, 55, 56, 63, 64, 65, 72, 73, 74],
+    [57, 58, 59, 66, 67, 68, 75, 76, 77],
+    [60, 61, 62, 69, 70, 71, 78, 79, 80]
+]
 
 
 background_color = pygame.Color(232, 196, 144)
@@ -52,19 +88,23 @@ def drawSudokuGrid():
         pygame.draw.line(screen, pygame.Color(8, 8, 8), (10,cor), (603,cor),width=2)
         pygame.draw.line(screen, pygame.Color(8, 8, 8), (cor,10), (cor,603),width=2)
         cor += 197
-    
+    pygame.draw.rect(screen, color, new_g_button, 1)
+    font = pygame.font.SysFont(None, 16)
+    new_g_text = font.render('NEW GAME', True, color)
+    screen.blit(new_g_text, (50,630))
 
+    
 def drawMarkButton():
     if marking: 
-        pygame.draw.rect(screen, color, mark_button, 6)
+        pygame.draw.rect(screen, color, mark_button, 5)
         font = pygame.font.SysFont(None, 17)
-        mark_text = font.render('mark', True, (255,0,0))
-        screen.blit(mark_text, (mark_button.x+5,mark_button.y+8))
+        mark_text = font.render('mark', True, (51, 29, 6))
+        screen.blit(mark_text, (mark_button.x+6,mark_button.y+8))
     else:
         pygame.draw.rect(screen, color, mark_button, 1)
         font = pygame.font.SysFont(None, 17)
         mark_text = font.render('mark', True, color)
-        screen.blit(mark_text, (mark_button.x+5,mark_button.y+8))
+        screen.blit(mark_text, (mark_button.x+6,mark_button.y+8))
 
 
 def difficulty_screen():
@@ -131,6 +171,20 @@ def print_numbers():
                 screen.blit(img, (x+27, y+25))
                 i+=1
     
+def get_section():
+    for section in quadrants:
+        if rect_list.index(selected_rect) in section:
+            return section
+
+def get_row():
+    for row in rows:
+        if rect_list.index(selected_rect) in row:
+            return row
+
+def get_column():
+    for col in columns:
+        if rect_list.index(selected_rect) in col:
+            return col
 
 def markups():
     a = 0
@@ -158,7 +212,26 @@ def markups():
                 else:
                     y += 22   
             a += 1
-             
+    
+def clean_marks(key_var):
+    current_section = get_section()
+    current_row = get_row()
+    current_column = get_column()
+
+    for index in current_section:
+        for index2 in range(len(marked_list[index])):
+            if marked_list[index][index2] == key_var:
+                marked_list[index][index2] = 0
+    for index in current_row:
+        for index2 in range(len(marked_list[index])):
+            if marked_list[index][index2] == key_var:
+                marked_list[index][index2] = 0
+    for index in current_column:
+        for index2 in range(len(marked_list[index])):
+            if marked_list[index][index2] == key_var:
+                marked_list[index][index2] = 0
+
+
 
 #main loop     
 
@@ -232,6 +305,7 @@ while running:
                                             nums[i]=key_var
                                             if nums[i] == true_nums[i]:
                                                 incor = 'correct'
+                                                clean_marks(key_var)
                                             else:
                                                 incor = 'input is incorrect!'
                                                 mistakes+=1
